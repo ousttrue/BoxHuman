942e5bb2f03a5f229c0781d78b232c918c9eeb86
ousttrue <ousttrue@gmail.com>
2024-01-10 21:13:55 +0900

    bones

--- src/App.svelte
+++ src/App.svelte
@@ -1,39 +1,68 @@
 <script lang="ts">
   import "svelte-golden-layout/css/themes/goldenlayout-light-theme.css";
   import GoldenLayout from "svelte-golden-layout";
-
+  import type { LayoutConfig } from "golden-layout";
   import Viewport from "./Viewport.svelte";
   import Settings from "./Settings.svelte";
 
-  const components = { Viewport, Settings };
+  const components: { [key: string]: any } = { Viewport, Settings };
 
-  const layout = {
+  const layout: LayoutConfig = {
     root: {
       type: "row",
       content: [
         {
           type: "component",
           componentType: "Viewport",
+          componentState: {},
+        },
+        {
+          title: "spine",
+          type: "component",
+          componentType: "Settings",
           componentState: {
-            someProp: 1,
-            anotherProp: 1,
+            bones: ["hips", "spine", "chest", "upperChest", "neck", "head"],
           },
         },
         {
+          title: "arm",
           type: "component",
           componentType: "Settings",
+          componentState: {
+            bones: ["shoulder", "upperArm", "lowerArm", "hand"],
+          },
         },
         {
+          title: "leg",
           type: "component",
           componentType: "Settings",
+          componentState: {
+            bones: ["upperLeg", "lowerLeg", "foot", "toes"],
+          },
         },
         {
+          title: "fingers",
           type: "component",
           componentType: "Settings",
-        },
-        {
-          type: "component",
-          componentType: "Settings",
+          componentState: {
+            bones: [
+              "thumbMetacarpal",
+              "thumbProximal",
+              "thumbDistal", // thumb
+              "indexProximal",
+              "indexIntermediate",
+              "indexDistal", // index
+              "middleProximal",
+              "middleIntermediate",
+              "middleDistal", // middle
+              "ringProximal",
+              "ringIntermediate",
+              "ringDistal", // ring
+              "littleProximal",
+              "littleIntermediate",
+              "littleDistal", // little
+            ],
+          },
         },
       ],
     },
--- src/Settings.svelte
+++ src/Settings.svelte
@@ -1,47 +1,24 @@
 <script lang="ts">
   import { Checkbox, Pane, ThemeUtils, Slider } from "svelte-tweakpane-ui";
 
-  let autoRotate: boolean = false;
-  let enableDamping: boolean = true;
-  let rotateSpeed: number = 1;
-  let zoomToCursor: boolean = false;
-  let zoomSpeed: number = 1;
-  let minPolarAngle: number = 0;
-  let maxPolarAngle: number = Math.PI;
-  let enableZoom: boolean = true;
+  export let bones: string[] = [];
+
+  console.log("componentState", bones);
+
+  const values: Record<string, number> = {};
+  for (const bone of bones) {
+    values[bone] = 1.0;
+  }
 </script>
 
 <Pane theme={ThemeUtils.presets.light} position="inline" title="OrbitControls">
-  <Checkbox bind:value={autoRotate} label="autoRotate" />
-  <Checkbox bind:value={enableDamping} label="enableDamping" />
-  <Checkbox bind:value={enableZoom} label="enableZoom" />
-  <Checkbox bind:value={zoomToCursor} label="zoomToCursor" />
-  <Slider
-    label="rotateSpeed"
-    bind:value={rotateSpeed}
-    min={0.1}
-    max={2}
-    step={0.1}
-  />
-  <Slider
-    label="zoomSpeed"
-    bind:value={zoomSpeed}
-    min={0.1}
-    max={2}
-    step={0.1}
-  />
-  <Slider
-    label="minPolarAngle"
-    bind:value={minPolarAngle}
-    min={0}
-    max={Math.PI}
-    step={0.1}
-  />
-  <Slider
-    label="maxPolarAngle"
-    bind:value={maxPolarAngle}
-    min={0}
-    max={Math.PI}
-    step={0.1}
-  />
+  {#each bones as bone}
+    <Slider
+      label={bone}
+      bind:value={values[bone]}
+      min={0.1}
+      max={2}
+      step={0.1}
+    />
+  {/each}
 </Pane>
